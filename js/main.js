c5ee1d8a9ab69ac239227c58fe8611ef0f3a1abd
SOHNYERAM <dpfka116@gmail.com>
2023-12-21 19:01:28 +0900

    오비맥주 스크롤 관련 mainpage 컨텐츠 scss 작업중

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const gnbCall = document.querySelector('.gnbCall');
 const moTitle = document.querySelector('.moTitle');
+// 스크롤 변수
+const sections = document.querySelectorAll('section');
+let positionArray = [];
 
 //햄버거버튼 활성화, 비활성화
 gnbCall.addEventListener('click', function (e) {
@@ -20,3 +23,23 @@ gnbCall.addEventListener('click', function (e) {
 });
 
 // 스크롤
+// offsetTop값을 구하기
+function setPosition() {
+	for (let el of sections) {
+		positionArray.push(el.offsetTop);
+	}
+}
+setPosition();
+console.log(positionArray); //[0:1340  1:2196  2:2843]
+
+window.addEventListener('scroll', () => {
+	let scroll = window.scrollY || window.pageYOffset;
+	sections.forEach((el, index) => {
+		if (scroll >= positionArray[index]) {
+			for (let el of sections) {
+				el.classList.remove('on');
+			}
+			sections[index].classList.add('on');
+		}
+	});
+});
